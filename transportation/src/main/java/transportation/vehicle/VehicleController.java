903ba05cf8bcb5c0ed95ba33d78975d210ed7f9a
Randy <rac154@txstate.edu>
2019-05-30 13:12:29 -0500

    Cleaning up some broken files

--- transportation/src/main/java/transportation/vehicle/VehicleController.java
+++ transportation/src/main/java/transportation/vehicle/VehicleController.java
@@ -1,7 +1,6 @@
 package transportation.vehicle;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
