5f516b43c8cd5672b42800d9adb5d34f75919dcc
ms3252 <49685464+ms3252@users.noreply.github.com>
2019-05-31 13:54:05 -0500

    extended servlet intializer

--- facility-spring-boot/src/main/java/com/revature/boot/FacilityBootApplication.java
+++ facility-spring-boot/src/main/java/com/revature/boot/FacilityBootApplication.java
@@ -2,11 +2,12 @@ package com.revature.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.revature.boot.*"})
-public class FacilityBootApplication {
+public class FacilityBootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacilityBootApplication.class, args);
