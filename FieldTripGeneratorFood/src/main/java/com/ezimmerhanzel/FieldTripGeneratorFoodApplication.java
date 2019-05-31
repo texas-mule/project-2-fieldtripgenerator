f23726ce09534830080aa9ec0e9c4104c11eb438
JynFlyn <ezimmerhanzel@tamu.edu>
2019-05-31 13:32:51 -0500

    Add files via upload

--- FieldTripGeneratorFood/src/main/java/com/ezimmerhanzel/FieldTripGeneratorFoodApplication.java
+++ FieldTripGeneratorFood/src/main/java/com/ezimmerhanzel/FieldTripGeneratorFoodApplication.java
@@ -2,9 +2,10 @@ package com.ezimmerhanzel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FieldTripGeneratorFoodApplication {
+public class FieldTripGeneratorFoodApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FieldTripGeneratorFoodApplication.class, args);
