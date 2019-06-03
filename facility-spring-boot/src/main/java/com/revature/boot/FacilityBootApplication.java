697a4d56c0601a8daf449a997d4432199019250b
ms3252 <michelle.schooley6@gmail.com>
2019-06-02 19:46:58 -0500

    edited application, changed properties

--- facility-spring-boot/src/main/java/com/revature/boot/FacilityBootApplication.java
+++ facility-spring-boot/src/main/java/com/revature/boot/FacilityBootApplication.java
@@ -2,6 +2,7 @@ package com.revature.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -9,8 +10,13 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.revature.boot.*"})
 public class FacilityBootApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FacilityBootApplication.class).properties("spring.config.name: facility-app");
+	}
 	public static void main(String[] args) {
-		SpringApplication.run(FacilityBootApplication.class, args);
+		System.setProperty("spring.config.name", "facility-app");
+		SpringApplication.run(FacilityBootApplication.class, args); 
 	}
 
 }
